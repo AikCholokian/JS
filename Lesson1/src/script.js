e616568f5ee862d8bc7260b093e7e625b169c429
Aik <wertoss@gmail.com>
2026-07-27 21:27:37 +0300

    Add basic JavaScript examples to script.js

---
+++ Lesson1/src/script.js
@@ -0,0 +1,38 @@
+console.log("Hello World");
+let x = 3.141592652;
+console.log(x);
+let y;
+console.log(y);
+const z = 2.5;
+console.log(z);
+x = 'hello';
+console.log(x);
+console.log(typeof (x));
+console.log(typeof (z));
+console.log(typeof (y));
+y = x;
+console.log(typeof (y), y);
+console.log('y=' + (y));
+console.log(`y=${y}`);
+let pi = 3.141592652;
+let a = `pi = ${pi}`;
+console.log(a);
+let result = 7 / 3;
+console.log(result);
+result = 7 % 5;
+console.log(result);
+console.log(`res=${10 % 3}`);
+let res = 5 + 7;
+console.log(res);
+res = '5' + '7';
+console.log(res);
+res = null - 5
+console.log(res);
+res = true + 5
+console.log(res);
+res=false*5
+console.log(res);
+res='hello'+5;
+console.log(res);
+res='hello'*5;
+console.log(res);
