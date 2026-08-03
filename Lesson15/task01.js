378be044c39b65d7247f7aa3c072e489e580fb86
AIK <wertoss@gmail.com>
2026-08-03 19:22:37 +0300

    Add user object and console log outputs in task01.js

--- Lesson15/task01.js
+++ Lesson15/task01.js
@@ -1,10 +1,15 @@
 console.log("Hello, World!");
-
-
-
-
-
-
-
-
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true,
+    email: "john@example.com",
+    city: "New York"
+}
+console.log(user.name);
+console.log(user.age);
+console.log(user.isAdmin);
+console.log(user.email);
+console.log(user.city);
+console.log(user);
 
