6fdc98cc9fd6271a0c64e5dfb3bd0a0b7638d608
AIK <wertoss@gmail.com>
2026-08-11 16:32:59 +0300

    Refactor homework tasks to include logic for checking happy numbers and add validation for 6-digit numbers

--- Lesson17/HomeWork.js
+++ Lesson17/HomeWork.js
@@ -1,7 +1,7 @@
 // hw17text
 /*
 1. Напишите программу,
-которая проверяет проверяет и печаттает вердикт ,
+которая проверяет и печаттает вердикт ,
 является ли целое положительное
 число любой длины счастливым попозиционно!
 ЕСЛИ  Сумма цифр на четных позициях равна
@@ -21,4 +21,73 @@
  
 712004- 7+1+2 != 0+0+4 не счастливое
 */
- 
+console.log("Задача 1");
+
+let number = 1210;
+let numberString = number.toString();
+
+let evenSum = 0;
+let oddSum = 0;
+
+for (let i = 0; i < numberString.length; i++) {
+  if (i % 2 === 0) {
+    evenSum += Number(numberString[i]);
+  } else {
+    oddSum += Number(numberString[i]);
+  }
+}
+
+let result = evenSum === oddSum ? "Счастливое число" : "Не счастливое число";
+console.log(result);
+
+console .log("Задача 2");
+
+let number2 = 123420;
+let number2String = number2.toString();
+if (number2String.length !== 6) {
+  console.log("Число не является 6-значным");
+}
+let leftSum = 0;
+let rightSum = 0;
+for (let i = 0; i < 3; i++) {
+  leftSum += Number(number2String[i]);
+  rightSum += Number(number2String[number2String.length - 1 - i]);
+}
+let result2 = leftSum === rightSum ? "Счастливое число" : "Не счастливое число";
+console.log(result2);
+
+console.log("Задача 3");
+
+let arrayNumbers = [];
+while (number>0) {
+    arrayNumbers.unshift(number % 10);
+    number = Math.floor(number / 10);
+}
+console.log(arrayNumbers);
+for (let i = 0; i < arrayNumbers.length; i++) {
+  if (i % 2 === 0) {
+    evenSum += Number(arrayNumbers[i]);
+  } else {
+    oddSum += Number(arrayNumbers[i]);
+  }
+}      
+let result3 = evenSum === oddSum ? "Счастливое число" : "Не счастливое число";
+console.log(result3);
+
+console.log("Задача 4");
+
+let arrayNumbers2 = [];
+while (number2>0) {
+    arrayNumbers2.unshift(number2 % 10);
+    number2 = Math.floor(number2 / 10);
+}
+console.log(arrayNumbers2);
+if (arrayNumbers2.length !== 6) {
+  console.log("Число не является 6-значным");
+}
+for (let i = 0; i < 3; i++) {
+  leftSum += Number(arrayNumbers2[i]);
+  rightSum += Number(arrayNumbers2[arrayNumbers2.length - 1 - i]);
+}
+let result4 = leftSum === rightSum ? "Счастливое число" : "Не счастливое число";
+console.log(result4);
