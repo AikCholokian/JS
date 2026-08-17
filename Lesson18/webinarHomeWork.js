cc494f606c2c8f5193399bd4ab515db49af5d17d
AIK <wertoss@gmail.com>
2026-08-17 18:52:50 +0300

    calculator

--- Lesson18/webinarHomeWork.js
+++ Lesson18/webinarHomeWork.js
@@ -1,3 +1,5 @@
+// Calculator
+
 function math(firstNumber){
     return {
         add: (secondNumber) => firstNumber + secondNumber,
@@ -6,5 +8,38 @@ function math(firstNumber){
         division: (secondNumber) => secondNumber === 0 ? "Ошибка: деление на ноль!" : firstNumber / secondNumber
     };
 }
+const calc = math(10);
+// console.log(calc.multiply(2));
+
+//Calculator Pro v 1.0
+
+function math(firstNumber){
+    let result = firstNumber;
+    return {
+        add: function(x) {
+            result += x;
+            return this
+        },
+        subtract: function(x) {
+            result -= x;
+            return this
+        },
+        multiply: function(x) {
+            result *= x;
+            return this
+        },
+        division: function(x) {
+            if (x === 0) {
+              return 'Ошибка: деление на ноль!'  
+            }
+            result /= x;
+            return this
+        },
+        res: function(){
+            return result
+        } 
+    };
+}    
+ 
 const calc = math(10);
 console.log(calc.multiply(2));
