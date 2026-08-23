97081fe70b7f83b3fa2754f87c985fc9a164a0f8
AIK <wertoss@gmail.com>
2026-08-23 19:05:24 +0300

    Add reverseArray function to reverse and log the primes array

--- Lesson20/task01.js
+++ Lesson20/task01.js
@@ -7,3 +7,13 @@ const printArray = (arr) => {
 
 const primes = [2,3,5,7,11,13,17,19,23,29];
 printArray(primes);
+
+const reverseArray = (arr) => {
+    for (let i=0, j=arr.length-1; i<j; i++, j--) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+};
+reverseArray(primes);
+console.log(primes);
